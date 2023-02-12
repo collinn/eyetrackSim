3e2c5c7df52d6cb2f116b62b9e4123118e136cd1
collin <collin@uiowa.edu>
2023-02-12 11:09:24 -0600

    ok i think i figured out what i want

--- analysis/dg_generate_pb_data.R
+++ analysis/dg_generate_pb_data.R
@@ -7,7 +7,7 @@ library(ggplot2)
 
 ## Tryin some new shit
 N <- 1000
-FBST <- TRUE # change fixation length
+FBST <- FALSE # change fixation length
 dg_sim_no_delay <- runSim_pb(nsub = N, ntrials = 300,
                              fnct = "doubleGauss", fbst = FBST,
                              omDelay = 0)
@@ -18,7 +18,7 @@ dg_sim_uniform <- runSim_pb(nsub = N, ntrials = 300,
                             fnct = "doubleGauss", fbst = FBST,
                             omDelay = unf_rv)
 
-wb <- function() rweibull(1, shape = 1.8, scale = 224.9)
+wb <- function() max(100, rweibull(1, shape = 1.8, scale = 224.9))
 makeActiveBinding("wb_rv", wb, .GlobalEnv)
 dg_sim_weibull <- runSim_pb(nsub = N, ntrials = 300,
                             fnct = "doubleGauss", fbst = FBST,
@@ -33,7 +33,7 @@ dg_fit_fix_no_delay <- bdotsFit(dg_sim_no_delay$trialData,
                                 time = "times",
                                 y = "looks",
                                 group = "group",
-                                curveType = doubleGauss_sac())
+                                curveType = doubleGauss_sac(startSamp = 15))
 cat("1\n")
 
 dg_fit_sac_no_delay <- bdotsFit(dg_sim_no_delay$fixations,
@@ -41,7 +41,7 @@ dg_fit_sac_no_delay <- bdotsFit(dg_sim_no_delay$fixations,
                                 time = "starttime",
                                 y = "looks",
                                 group = "group",
-                                curveType = doubleGauss_sac())
+                                curveType = doubleGauss_sac(startSamp = 15))
 cat("2\n")
 dg_sim_uniform$trialData[, times := times - 200L]
 dg_sim_uniform$fixations[, starttime := starttime - 200L]
@@ -50,14 +50,14 @@ dg_fit_fix_uniform <- bdotsFit(dg_sim_uniform$trialData,
                                time = "times",
                                y = "looks",
                                group = "group",
-                               curveType = doubleGauss_sac())
+                               curveType = doubleGauss_sac(startSamp = 15))
 cat("3\n")
 dg_fit_sac_uniform <- bdotsFit(dg_sim_uniform$fixations,
                                subject = "id",
                                time = "starttime",
                                y = "looks",
                                group = "group",
-                               curveType = doubleGauss_sac())
+                               curveType = doubleGauss_sac(startSamp = 15))
 cat("4\n")
 dg_sim_weibull$trialData[, times := times - 200L]
 dg_sim_weibull$fixations[, starttime := starttime - 200L]
@@ -66,14 +66,14 @@ dg_fit_fix_weibull <- bdotsFit(dg_sim_weibull$trialData,
                                time = "times",
                                y = "looks",
                                group = "group",
-                               curveType = doubleGauss_sac())
+                               curveType = doubleGauss_sac(startSamp = 15))
 cat("5\n")
 dg_fit_sac_weibull <- bdotsFit(dg_sim_weibull$fixations,
                                subject = "id",
                                time = "starttime",
                                y = "looks",
                                group = "group",
-                               curveType = doubleGauss_sac())
+                               curveType = doubleGauss_sac(startSamp = 15))
 
 if (FBST) {
   save.image(file = "dg_pb_data_sim_fbst.RData")
--- analysis/generate_pb_sim_data.R
+++ analysis/generate_pb_sim_data.R
@@ -2,36 +2,45 @@ library(eyetrackSim)
 library(bdots)
 library(ggplot2)
 
-
-# sim_fixed <- runSim(nsub = 1000, ntrials = 300,
-#                     fnct = "logistic", fbst = FALSE,
-#                     sacDelay = 0)
-# cat("-1\n")
-#
-# sim_random <- runSim(nsub = 1000, ntrials = 300,
-#                      fnct = "logistic", fbst = FALSE,
-#                      sacDelay = NULL)
-# cat("0\n")
-
 ## Tryin some new shit
+
+rrbeta <- function(n=1, s1=2, s2=0.55, p=20, q=250) {
+  b <- rbeta(n, s1, s2)
+  y <- b*(q-p) + p
+  y
+}
+
+
 N <- 1000
+nt <- 300
 FBST <- FALSE # change fixation length
-sim_no_delay <- runSim_pb(nsub = N, ntrials = 300,
-                    fnct = "logistic", fbst = FBST,
-                    omDelay = 0)
+sim_no_delay <- runSim_pb(nsub = N, ntrials = nt,
+                          fnct = "logistic", fbst = FBST,
+                          omDelay = 0)
 
-unf <- function() runif(1, min = 100, max = 300)
+unf <- function() runif(1, min = 100, max = 300) #abs(rnorm(1, 200, sd = 30))
 makeActiveBinding("unf_rv", unf, .GlobalEnv)
-sim_uniform <- runSim_pb(nsub = N, ntrials = 300,
-                    fnct = "logistic", fbst = FBST,
-                    omDelay = unf_rv)
+sim_uniform <- runSim_pb(nsub = N, ntrials = nt,
+                         fnct = "logistic", fbst = FBST,
+                         omDelay = unf_rv)
 
-wb <- function() rweibull(1, shape = 1.8, scale = 224.9)
+wb <- function() max(100, rweibull(1, shape = 1.8, scale = 224.9))
 makeActiveBinding("wb_rv", wb, .GlobalEnv)
-sim_weibull <- runSim_pb(nsub = N, ntrials = 300,
-                      fnct = "logistic", fbst = FBST,
-                      omDelay = wb_rv)
+sim_weibull <- runSim_pb(nsub = N, ntrials = nt,
+                         fnct = "logistic", fbst = FBST,
+                         omDelay = wb_rv)
 
+nn <- function() max(100, rnorm(1, 200, 15)) #abs(rnorm(1, 200, sd = 30))
+makeActiveBinding("nn_rv", nn, .GlobalEnv)
+sim_normal <- runSim_pb(nsub = N, ntrials = nt,
+                        fnct = "logistic", fbst = FBST,
+                        omDelay = nn_rv)
+
+bbe <- function() rrbeta(1, 2, 1, 100, 250) #rweibull(1, shape = 1.8, scale = 224.9)
+makeActiveBinding("bb_rv", bbe, .GlobalEnv)
+sim_beta <- runSim_pb(nsub = N, ntrials = nt,
+                      fnct = "logistic", fbst = FBST,
+                      omDelay = bb_rv)
 
 
 ## Now using same starting parameters for all (as to not bias things greatly)
@@ -91,8 +100,42 @@ fit_sac_weibull <- bdotsFit(sim_weibull$fixations,
                             group = "group",
                             curveType = logistic_sac())
 
+cat("6\n")
+sim_normal$trialData[, times := times - 200L]
+sim_normal$fixations[, starttime := starttime - 200L]
+fit_fix_normal <- bdotsFit(sim_normal$trialData,
+                            subject = "id",
+                            time = "times",
+                            y = "looks",
+                            group = "group",
+                            curveType = logistic_sac())
+cat("7\n")
+fit_sac_normal <- bdotsFit(sim_normal$fixations,
+                            subject = "id",
+                            time = "starttime",
+                            y = "looks",
+                            group = "group",
+                            curveType = logistic_sac())
+
+cat("8\n")
+sim_beta$trialData[, times := times - 200L]
+sim_beta$fixations[, starttime := starttime - 200L]
+fit_fix_beta <- bdotsFit(sim_beta$trialData,
+                           subject = "id",
+                           time = "times",
+                           y = "looks",
+                           group = "group",
+                           curveType = logistic_sac())
+cat("9\n")
+fit_sac_beta <- bdotsFit(sim_beta$fixations,
+                           subject = "id",
+                           time = "starttime",
+                           y = "looks",
+                           group = "group",
+                           curveType = logistic_sac())
+
 if (FBST) {
-  save.image(file = "pb_data_sim_fbst_no_start_par.RData")
+  save.image(file = "pb_data_sim_fbst_normal.RData")
 } else {
-  save.image(file = "pb_data_sim_no_fbst_no_start_par.RData")
+  save.image(file = "pb_data_sim_no_fbst_normal.RData")
 }
